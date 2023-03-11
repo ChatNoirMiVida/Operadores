e9e902cd447e30790f7ba1e282e34a2bd3be9f84
Anna <127609375+ChatNoirMiVida@users.noreply.github.com>
2023-03-11 09:01:43 -0600

    Add files via upload

---
+++ Operadores.js
@@ -0,0 +1,66 @@
+/* // El % nos muestra el restante de la division // 
+let numero1 = 4;
+let numero2 = 3;
+console.log(numero1 % 2);
+console.log(numero2 % 2);
+
+//Operador igual, nos va a mostrar "false o true" segun el caso//
+let numero3 = 14;
+let numero4 = 13;
+document.write(numero3 == numero4);
+document.write("<br>");
+
+//Operador diferente que, nos va a mostrar "false o true" segun el caso//
+let numero5 = 14;
+let numero6 = 18;
+document.write(numero5 != numero6);
+document.write("<br>");
+
+//Operador mayor que, nos va a mostrar "false o true" segun el caso//
+let numero7 = 18;
+let numero8 = 15;
+document.write(numero7 > numero8);
+document.write("<br>");
+
+//Operador menor que, nos va a mostrar "false o true" segun el caso//
+let numero9 = 12;
+let numero10 = 13;
+document.write(numero9 < numero10);
+document.write("<br>");
+
+//Operador mayor o igual, nos va a mostrar "false o true" segun el caso//
+let numero11 = 12;
+let numero12 = 13;
+document.write(numero11 >= numero12);
+document.write("<br>");
+
+//Operador menor o igual, nos va a mostrar "false o true" segun el caso//
+let numero13 = 12;
+let numero14 = 13;
+document.write(numero13 <= numero14);
+document.write("<br>");
+
+//Operador identico, nos va a mostrar "false o true" segun el caso//
+let numero15 = 12;
+let numero16 = 13;
+document.write(numero15 === numero16);
+document.write("<br>");
+
+//Operador distinto, nos va a mostrar "false o true" segun el caso//
+let numero17 = 12;
+let numero18 = 13;
+document.write(numero17 !== numero18);
+document.write("<br>");
+ */
+
+//El primero nos indica que es igual mientras que el segundo nos indica que no es lo mismmo ya que aunque es el mismo valor las comillas hacen que se muestre como texto//
+let number = "1";
+let number2 = 1;
+document.write(number == number2);
+document.write(number === number2);
+document.write("<br>");
+
+let number3 = "1";
+let number4 = 1;
+document.write(number3 == number4);
+document.write(number3 !== number4);
---
+++ Practica.js
@@ -0,0 +1,33 @@
+// prompt sirve para guardar todo tipo de texto o números 
+/* let nombre = prompt("¿Cuál es tu nombre?");
+console.log(nombre);
+
+let edad = prompt("¿Cuál es tu edad?");
+console.log(edad); */
+
+// El operador de asignación = se utiliza para asignar un valor a una variable 
+
+/* let x = 2;
+const y = 3;
+console.log(x);
+console.log(x = y + 1);
+console.log(x = x * y); 
+ */
+// Pedir dos números con prompt
+
+
+// CONCATENAR
+
+let nombre1 = prompt("¿Como te llamas?");
+let numero0 = prompt("Hola " + nombre1 + " ,dame tu primer numero");
+
+let numero2 = prompt(nombre1 + " ,dame tu segundo numero");
+console.log(numero2);
+
+let suma = (parseInt(numero0)) + (parseInt(numero2));
+console.log(suma); 
+
+
+console.log(numero0 / numero2);
+console.log(numero0 * numero2);
+ 
